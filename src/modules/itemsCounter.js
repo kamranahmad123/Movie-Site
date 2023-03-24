a3479144e61dc7b9cd3a6c82f37cb174bffaac24
kamranahmad123 <kamrankhan.ahmad2@yahoo.com>
2023-03-24 10:00:14 -0700

    resolve linter errors

--- src/modules/itemsCounter.js
+++ src/modules/itemsCounter.js
@@ -1,5 +1,5 @@
 const itemsCounter = document.getElementById('showNumber');
 
-export default function ItemsCount (count) {
+export default function ItemsCount(count) {
   itemsCounter.innerHTML = `Total TV-shows(${count})`;
 }
