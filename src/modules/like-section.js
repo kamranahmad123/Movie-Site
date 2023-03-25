be471c46e9708165f6ac6d0b2edc682752eeb9cd
Atril33 <118503473+Atril33@users.noreply.github.com>
2023-03-25 15:45:02 +0500

    Update code Files

--- src/modules/like-section.js
+++ src/modules/like-section.js
@@ -1,14 +1,14 @@
-function sendData(count) {
+const sendData = (count) => {
   fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/iSougRnGfvufqYKBqkTu/likes/', {
     method: 'POST',
     body: JSON.stringify({ item_id: `item${count}` }),
     headers: { 'Content-type': 'application/json; charset=UTF-8' },
   });
-  function reload1() {
+  const reload1 = () => {
     window.location.reload();
-  }
+  };
   setTimeout(reload1, 1300);
-}
+};
 
 const getLikeData = async (count) => {
   const res = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/iSougRnGfvufqYKBqkTu/likes');
